9df6a1cbc31c89b57b5704cbbc76fb1bef6e660f
Pronob <diptasaha002@gmail.com>
2026-04-02 18:01:52 +0600

    current user function added

--- security.py
+++ security.py
@@ -4,6 +4,11 @@ from passlib.context import CryptContext
 import os
 import bcrypt
 from dotenv import load_dotenv
+from database import get_db
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer
 
 SECRET_KEY=os.getenv("SECRET_KEY", "abc123@#$%^&*()")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
@@ -31,4 +36,27 @@ def create_access_token(data:dict)->str:
     copy.update({"exp":expire})
     encoded_jwt=jwt.encode(copy, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+
+token_schema=OAuth2PasswordBearer(tokenUrl="/login")
+
+def get_current_user(token:Depends(token_schema),db:Session=Depends(get_db)):
+    credential_exception=HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload=jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id=payload.get("sub")
+        if user_id is None:
+            raise credential_exception
+    except jwt.PyJWTError:
+        raise credential_exception
+    user=db.query(models.User).filter(models.User.id==user_id).first()
+    if user is None:
+        raise ceredential_exception
+    return user
+    
     
